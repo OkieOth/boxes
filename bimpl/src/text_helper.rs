c4ef323aa17431a2d3fcc7978c93e8ba859e9040
Eiko Thomas <eiko.thomas@gmail.com>
2024-04-14 23:30:27 +0200

    add font group enum

--- bimpl/src/text_helper.rs
+++ bimpl/src/text_helper.rs
@@ -1,5 +1,14 @@
 use rusttype::{point, Font, Scale};
 
-pub fn get_text_bbox(text: &str, font_size: usize) -> (usize, usize) {
+pub enum FontGroup {
+    Sans,
+    SansItalic,
+    SansBold,
+    SansBoldItalic,
+    Mono,
+    MonoItalic,
+}
+
+pub fn get_text_bbox(text: &str, font_size: usize, font_group: FontGroup) -> (usize, usize) {
     
 }
